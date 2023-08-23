79952c56c475d4d47e6748deac726c7917d65644
Radhika <radhikakumari0508@gmail.com>
2023-08-23 13:01:13 +0530

    installed react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+
+
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={ <Home />} />
+    <Route path='/starred' element={<Starred />}  />
 
-    </div>
+    <Route path='*' element={<div>Not found</div>}  />
+
+    
+      {/* <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="teams" element={<Teams />}>
+          <Route path=":teamId" element={<Team />} />
+          <Route path="new" element={<NewTeamForm />} />
+          <Route index element={<LeagueStandings />} />
+        </Route>
+      </Route>
+      <Route element={<PageLayout />}>
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/tos" element={<Tos />} />
+      </Route>
+      <Route path="contact-us" element={<Contact />} /> */}
+    </Routes>
+  </BrowserRouter>
   );
 }
 
