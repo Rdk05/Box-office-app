62fa29473ebe363626f4658303902db6761b064a
Radhika <radhikakumari0508@gmail.com>
2023-09-08 23:51:15 +0530

    used useQuery on Home page to fetch search data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,42 @@
 import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { searchForShows, searchForPeople } from '../api/tvmaze'; 
 import SearchForm from '../components/SearchForm';
 import ShowGrid from '../components/shows/ShowGrid';
 import ActorGrid from '../components/actors/ActorGrid';
 
 const Home = () => {
+    const [filter, setFilter] = useState(null);
+
+    const { data: apiData, error: apiDataError } = useQuery({
+        queryKey: ['search', filter],
+        queryFn: () => filter.searchOption === 'shows' ? searchForShows(filter.q) : searchForPeople(filter.q),
+        enabled: !!filter,
+    });
   
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null);
+//   const [apiData, setApiData] = useState(null);
+//   const [apiDataError, setApiDataError] = useState(null);
 
   const onSearch = async ({ q, searchOption}) => {
-    try {
-      setApiDataError(null);
 
-      let result;
+    setFilter({ q, searchOption })
 
-      if(searchOption === 'shows'){
-          result = await searchForShows(q);
-      }
-      else{
-          result = await searchForPeople(q);
-        }
+    // try {
+    //   setApiDataError(null);
+
+    //   let result;
+
+    //   if(searchOption === 'shows'){
+    //       result = await searchForShows(q);
+    //   }
+    //   else{
+    //       result = await searchForPeople(q);
+    //     }
         
-     setApiData(result);
-    } catch (error) {
-      setApiDataError(error);
-    }
+    //  setApiData(result);
+    // } catch (error) {
+    //   setApiDataError(error);
+    // }
   };
 
   const renderApiData = () => {
