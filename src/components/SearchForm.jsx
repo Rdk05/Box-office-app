3ae4b762264f1d074ddf4ff8e0d1399524ee6174
Radhika <radhikakumari0508@gmail.com>
2023-09-08 17:37:11 +0530

    fetch data inside show components

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -4,6 +4,7 @@ const SearchForm = ({ onSearch }) => {
     const [searchStr, setSearchStr] = useState('');
   const [searchOption, setSearchOption] = useState('shows');
 
+
     const onSearchInputChange = ev => {
         setSearchStr(ev.target.value);
       };
